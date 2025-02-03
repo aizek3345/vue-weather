7985bba7c53fca90140c163d816c0cda67b51926
egorka <egorka_988@mail.ru>
2025-02-03 20:44:15 +0300

    Немного html вёрстки

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="weather">
+    <div class="container">
+
+      <div class="card weather-form">
+        <input type="text" 
+          class="weather-form-input"
+          placeholder="Enter city">
+        <button class="weather-form-button">Search</button>        
+      </div>
+
+      <div class="weather-info">
+        <div class="weather-info-text">
+          <p class="card">Kostroma</p>
+          <p class="card">5°C</p>
+          <p class="card">Sunny</p>
+        </div>
+      </div>
+      
+      <div class="weather-bg">
+        <div>
+          <img src="./assets/bg.png" alt="App Background">
+          <!-- продолжить -->
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
